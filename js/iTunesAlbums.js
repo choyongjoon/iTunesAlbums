cd5b221f5d53b3b661c5fde2330c2914284b33ec
choyongjoon <w2000wkd@gmail.com>
2014-01-13 23:03:22 +0900

    Added select/unselect album
    - enlarge/reduce album image
    - hide/unhide artist name

--- js/iTunesAlbums.js
+++ js/iTunesAlbums.js
@@ -1,8 +1,44 @@
-$(function() { 
- 
-console.log("adfae");
-    $("#album1").click(function() {  
-    		console.print("adfae");
-        $(".album-container").toggleClass(".album-container-selected");  
-    });  
+/*!
+ * iTunesAlbums.js
+ */
+
+$(document).ready(function() {
+
+	var selectedAlbumContainer;
+
+  $(".img-album").click(function() { // when album image is selected
+  	albumContainer = $(this).parent();
+  	//console.log(selectedAlbumContainer);
+  	//console.log(albumContainer);
+  	if(selectedAlbumContainer!=undefined){
+	  	if(selectedAlbumContainer.is(albumContainer)){
+	  		unselectAlbum(albumContainer);
+	  		$(".album-plate").addClass("hidden");
+	  	}else{
+				unselectAlbum(selectedAlbumContainer);
+				selectAlbum(albumContainer);
+	  	}
+  	}else{
+    	selectAlbum(albumContainer);
+    	$(".album-plate").removeClass("hidden");
+		}
+  });
+  
+  $(".close-button").click(function() {
+  	unselectAlbum(selectedAlbumContainer);
+  	$(".album-plate").toggleClass("hidden");
+  });
+  
+  function selectAlbum(albumContainer){
+		albumContainer.addClass("selected"); // enlarge album image
+		albumContainer.find(".artist-name").addClass("hidden"); // hide artist name
+		selectedAlbumContainer = albumContainer;
+  };
+  
+  function unselectAlbum(albumContainer){
+	  albumContainer.removeClass("selected"); // reduce album image
+		albumContainer.find(".artist-name").removeClass("hidden"); // show artist name
+		selectedAlbumContainer = undefined;
+  };
+  
 });  
